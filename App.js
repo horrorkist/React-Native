fdfd993f50485a65d690f0c11fc21a610febc017
horrorkist <horrorkist@gmail.com>
2022-07-22 05:57:33 +0900

    useAssets & useFonts

--- App.js
+++ App.js
@@ -1,50 +1,23 @@
 import React, { useCallback, useEffect, useState } from "react";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Image, View, Text } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    try {
-      const fonts = loadFonts([Ionicons.font]);
-      const images = loadImages(
-        [require("./assets/659096.jpg")],
-        "http://owenfood.speedgabia.com/cr/cr_008/coming_event_6.jpg"
-      );
-      await Promise.all([...fonts, ...images]);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setReady(true);
-    }
-  };
-  useEffect(() => {
-    startLoading();
-  }, []);
+  const [assets] = useAssets([require("./assets/659096.jpg")]);
+  const [fonts] = Font.useFonts(Ionicons.font);
 
   const onLayoutRootView = useCallback(async () => {
-    if (ready) {
+    if (assets && fonts) {
       await SplashScreen.hideAsync();
     }
-  }, [ready]);
+  }, [assets, fonts]);
 
-  if (!ready) return null;
+  if (!assets || !fonts) return null;
 
   return (
     <View onLayout={onLayoutRootView}>
