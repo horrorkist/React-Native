6dba37ed1fdfbe2a36f358d7935e091cf7b999e8
horrorkist <horrorkist@gmail.com>
2022-07-22 05:51:12 +0900

    Expo Splash Screen

--- App.js
+++ App.js
@@ -1,27 +1,54 @@
-import React, { useState } from "react";
-import AppLoading from "expo-app-loading";
+import React, { useCallback, useEffect, useState } from "react";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
-import { Image } from "react-native";
+import { Image, View, Text } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    // await Asset.loadAsync(require("./some-local-image.png"));
-    // await Image.prefetch("someOnlineImageUrl");
+    try {
+      const fonts = loadFonts([Ionicons.font]);
+      const images = loadImages(
+        [require("./assets/659096.jpg")],
+        "http://owenfood.speedgabia.com/cr/cr_008/coming_event_6.jpg"
+      );
+      await Promise.all([...fonts, ...images]);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setReady(true);
+    }
   };
+  useEffect(() => {
+    startLoading();
+  }, []);
 
-  if (!ready)
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      ></AppLoading>
-    );
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
 
-  return null;
+  if (!ready) return null;
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <Text>splash screen</Text>
+    </View>
+  );
 }
